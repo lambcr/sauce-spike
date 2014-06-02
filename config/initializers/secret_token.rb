7bf986d0075ed1f464dde150e928a194ede9b1a8
Chris Lamb <lamb.chrisr@gmail.com>
2014-06-02 16:38:57 -0400

    Initial spike app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SauceSpike::Application.config.secret_key_base = '9bd7a7dc6b32ee4b982253feeefd97f7221809b66c8c4ed5b51dbe7fa93dc15acc0986e9347418a2b981d43b2ba7f89a0a65e8ed87eb902a30f7be4c06aee79b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SauceSpike::Application.config.session_store :cookie_store, key: '_sauce-spike_session'
